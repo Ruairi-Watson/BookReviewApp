52a39d339d98bd25eb483adde51d0e646f685715
Ruairi-Watson <x21391571@student.ncirl.ie>
2024-12-20 19:10:29 +0000

    Fixed the colouring

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
@@ -14,32 +15,33 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to books_path, notice: "Book was successfully created."
+      redirect_to @book, notice: 'Book was successfully created.'
     else
       render :new
     end
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to books_path, notice: "Book was successfully updated."
+      redirect_to @book, notice: 'Book was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path, notice: "Book was successfully deleted."
+    redirect_to books_path, notice: 'Book was successfully deleted.'
   end
 
   private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 
   def book_params
     params.require(:book).permit(:title, :author, :description)
