a4b94dd708e314ddd930607734b14b1e795beeb8
Ruairi-Watson <x21391571@student.ncirl.ie>
2024-12-20 20:52:13 +0000

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "books#index"
-  resources :books
+  root "books#index" # Sets the root path to the books index page WORK?
+  resources :books   # Automatically generates all CRUD routes for the books resource DOESNT WORK 
 end
